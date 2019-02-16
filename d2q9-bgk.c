ce6315cde422f7eba24c290b484aea166f0d3633
MichaelRol <michael.rollins@hotmail.co.uk>
2019-02-16 23:06:59 +0000

    Stepping by 2 seems optimal

--- d2q9-bgk.c
+++ d2q9-bgk.c
@@ -154,11 +154,10 @@ int main(int argc, char* argv[])
   gettimeofday(&timstr, NULL);
   tic = timstr.tv_sec + (timstr.tv_usec / 1000000.0);
 
-  for (int tt = 0; tt < params.maxIters; tt += 3)
+  for (int tt = 0; tt < params.maxIters; tt += 2)
   {
     av_vels[tt] = timestep(params, cells, tmp_cells, obstacles);
     av_vels[tt+1] = timestep(params, cells, tmp_cells, obstacles);
-    av_vels[tt+2] = timestep(params, cells, tmp_cells, obstacles);
 #ifdef DEBUG
     printf("==timestep: %d==\n", tt);
     printf("av velocity: %.12E\n", av_vels[tt]);
